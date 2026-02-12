794c25907cc293365612e6d88ae25ef4c461d3c6
yu <yul1024@outlook.com>
2026-02-12 18:48:41 +0800

    test: add tests

---
+++ tests/unit_tests/test_gateway/test_litellm/test_litellm_proxy_launcher.py
@@ -0,0 +1,58 @@
+"""
+测试 litellm proxy launcher 的运行。
+"""
+
+from __future__ import annotations
+import pytest
+from loguru import logger
+
+from openai import (
+    OpenAI,
+)
+from langchain_openai import (
+    ChatOpenAI,
+    OpenAIEmbeddings,
+)
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class TestLiteLLMProxyLauncher:
+    def test_raw_ollama(
+        self,
+    ):
+        client = OpenAI(
+            base_url='http://127.0.0.1:11434/v1/',
+            api_key='none',
+        )
+        response = client.chat.completions.create(
+            model="qwen2.5:0.5b",
+            messages=[{"role": "user", "content": "What is your model id?"}]
+        )
+        logger.info(f"Raw Response: \n{response}")
+
+    def test_litellm_llm(
+        self,
+    ):
+        client = OpenAI(
+            base_url='http://127.0.0.1:4000',
+            api_key='none',
+        )
+        response = client.chat.completions.create(
+            model="ollama-llm",
+            messages=[{"role": "user", "content": "What is your model id?"}]
+        )
+        logger.info(f"LiteLLM Response: \n{response}")
+
+    def test_litellm_embedding(
+        self,
+    ):
+        embedding_model = OpenAIEmbeddings(
+            model="ollama-embedding",  # 对应你在 LiteLLM 中定义的模型名
+            base_url="http://127.0.0.1:4000",  # LiteLLM 端口
+            api_key="none",
+        )
+        embedding = embedding_model.embed_query('haha')
+        logger.info(f"LiteLLM Embedding: \n{embedding}")
+
