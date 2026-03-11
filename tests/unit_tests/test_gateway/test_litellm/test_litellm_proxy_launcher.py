d70e9a4b27d110bbea62a4a200d88fb0768dacc4
yu <yul1024@outlook.com>
2026-03-11 10:12:17 +0800

    test: add tests

--- tests/unit_tests/test_gateway/test_litellm/test_litellm_proxy_launcher.py
+++ tests/unit_tests/test_gateway/test_litellm/test_litellm_proxy_launcher.py
@@ -9,65 +9,70 @@ from loguru import logger
 from openai import (
     OpenAI,
 )
-from langchain_openai import (
-    ChatOpenAI,
-    OpenAIEmbeddings,
-)
+# from langchain_openai import (
+#     ChatOpenAI,
+#     OpenAIEmbeddings,
+# )
 
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
 
-class TestLiteLLMProxyLauncher:
-    def test_raw_ollama(
-        self,
-    ):
-        client = OpenAI(
-            base_url='http://127.0.0.1:11434/v1/',
-            api_key='none',
-        )
-        response = client.chat.completions.create(
-            model="qwen2.5:0.5b",
-            messages=[{"role": "user", "content": "What is your model id?"}]
-        )
-        logger.info(f"Raw Ollama Response: \n{response}")
+@pytest.fixture(name='openai_client')
+def make_openai_client():
+    client = OpenAI(
+        base_url='http://127.0.0.1:4000',
+        api_key='none',
+    )
+    return client
 
-    def test_ollama_llm(
-        self,
-    ):
-        client = OpenAI(
-            base_url='http://127.0.0.1:4000',
-            api_key='none',
-        )
-        response = client.chat.completions.create(
-            model="ollama-llm",
-            messages=[{"role": "user", "content": "What is your model id?"}]
-        )
-        logger.info(f"Ollama Response: \n{response}")
 
-    def test_ollama_embedding(
-        self,
-    ):
-        embedding_model = OpenAIEmbeddings(
-            model="ollama-embedding",
-            base_url="http://127.0.0.1:4000",
-            api_key='none',
-        )
-        embedding = embedding_model.embed_query('haha')
-        logger.info(f"Ollama Embedding: \n{embedding}")
+def test_raw_ollama():
+    client = OpenAI(
+        base_url='http://127.0.0.1:11434/v1/',
+        api_key='none',
+    )
+    response = client.chat.completions.create(
+        model="qwen2.5:0.5b",
+        messages=[{"role": "user", "content": "What is your model id?"}]
+    )
+    logger.info(f"Raw Ollama Response: \n{response}")
 
-    def test_vllm_llm(
-        self,
-    ):
-        ...
 
-    def test_vllm_embedding(
-        self,
-    ):
-        ...
+def test_ollama_llm(
+    openai_client: OpenAI,
+):
+    response = openai_client.chat.completions.create(
+        model="ollama-llm",
+        messages=[{"role": "user", "content": "What is your model id?"}]
+    )
+    logger.info(f"Ollama Response: \n{response}")
 
-    def test_dashscope_llm(
-        self,
-    ):
-        ...
+
+def test_ollama_embedding(
+    openai_client: OpenAI,
+):
+    response = openai_client.embeddings.create(
+        input='haha',
+        model='ollama-embedding',
+    )
+    logger.info(f"Ollama Embedding Response: \n{response}")
+
+
+# def test_vllm_llm(
+#     openai_client: OpenAI,
+# ):
+#     ...
+#
+#
+# def test_vllm_embedding(
+#     openai_client: OpenAI,
+# ):
+#     ...
+#
+#
+# def test_dashscope_llm(
+#     openai_client: OpenAI,
+# ):
+#     ...
 
