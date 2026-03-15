a57bf45f19d42c4f0fef939dd9e993e945f7c46c
yu <yul1024@outlook.com>
2026-03-15 21:17:43 +0800

    feat: add huggingface downloader

--- model_foundations/transformers/hf_downloader.py
+++ model_foundations/transformers/hf_downloader.py
@@ -37,6 +37,12 @@ from typing import TYPE_CHECKING, Sequence, Literal
 
 
 class HFDownloaderInterface(ABC):
+    """
+    必要的下载器需要实现的方法。
+
+    repo_type: Literal['model', 'dataset', 'space',]
+    这里必要的实现为 model 和 dataset 。
+    """
     @abstractmethod
     def download_models(self, *args, **kwargs):
         """
@@ -51,6 +57,11 @@ class HFDownloaderInterface(ABC):
 
 
 class HFDownloader(HFDownloaderInterface):
+    """
+    通过 huggingface_hub 的 snapshot_download 方法实现对仓库的下载。
+
+    默认使用多线程加速下载。
+    """
     def __init__(
         self,
         local_model_dir: str,
@@ -59,24 +70,29 @@ class HFDownloader(HFDownloaderInterface):
         is_only_torch: bool,
         repo_concurrency: int,
         file_concurrency: int,
+        hf_home: str | None,
+        hf_endpoint: str | None,
     ):
         """
         Args:
-            local_model_dir: 本地存储仓库这个文件夹的路径。
-            local_dataset_dir: 本地存储仓库这个文件夹的路径。
-            is_only_torch: 是否仅下载torch相关的文件。默认为了空间和速度，仅下载torch相关。
+            local_model_dir (str): 本地存储仓库这个文件夹的路径。
+            local_dataset_dir (str): 本地存储仓库这个文件夹的路径。
+            is_only_torch (bool): 是否仅下载torch相关的文件。默认为了空间和速度，仅下载torch相关。
             # is_batch_download: 是否使用多线程批量下载。多线程下载现在是默认行为。
         """
         self.local_model_dir = Path(local_model_dir)
         self.local_dataset_dir = Path(local_dataset_dir)
         self.cache_dir = Path(cache_dir)
         self._is_only_torch = is_only_torch
-        # self.is_batch_download = is_batch_download
         self._repo_concurrency = repo_concurrency
         self._file_concurrency = file_concurrency
+        # unstable: set env var
+        self.set_environment_variables(
+            hf_home=hf_home,
+            hf_endpoint=hf_endpoint,
+        )
 
-        self.set_environment_variables()
-
+    # ==== 暴露方法。 ====
     def download_models(
         self,
         repo_ids: Sequence[str],
@@ -84,6 +100,7 @@ class HFDownloader(HFDownloaderInterface):
         with ThreadPoolExecutor(max_workers=self._repo_concurrency) as executor:
             executor.map(self.download_datasets, repo_ids)
 
+    # ==== 暴露方法。 ====
     def download_datasets(
         self,
         repo_ids: Sequence[str],
@@ -91,28 +108,25 @@ class HFDownloader(HFDownloaderInterface):
         with ThreadPoolExecutor(max_workers=self._repo_concurrency) as executor:
             executor.map(self.download_datasets, repo_ids)
 
-    # def try_download_snapshot(
-    #     self,
-    #     repo_id: str,
-    #     repo_type: Literal['model', 'dataset', 'space',]
-    # ) -> None:
-    #     if self._is_only_torch:
-    #         allow_patterns = [
-    #             '*.pt', '*.pth', '*.bin',
-    #             '*.json', '*.txt', '*.md',
-    #             '*.safetensors',
-    #             # '*.tar'
-    #         ]  # 我不断在检查和总结的torch相关的文件。
-
+    # ==== 重要方法。 ====
     def try_download_model(
         self,
         repo_id: str,
     ) -> None:
         """
-        从huggingface上下载模型。
+        从 huggingface 上下载模型。
+
+        States:
+            _is_only_torch (bool): 是否仅下载 torch 相关文件。
+                实现方法为指定常见 allow_patterns ，其他方法也可通过指定 ignore_patterns 。
 
         Args:
-            repo_id: huggingface上仓库的id，一般是 "用户名/仓库名" ，可以自动复制的。
+            repo_id (str): huggingface 上仓库的 id ，一般是 "用户名/仓库名" ，可自动复制。
+
+        Returns:
+            None: 尝试执行下载模型。
+                报错会以日志进行记录，但不会中断下载任务。
+                该方法约定被多线程方法批量运行，各下载任务独立进行，可重复启动和恢复。
         """
         try:
             # 下载模型
@@ -127,11 +141,12 @@ class HFDownloader(HFDownloaderInterface):
                         '*.json', '*.txt', '*.md',
                         '*.safetensors',
                         # '*.tar'
-                    ],  # 我不断在检查和总结的torch相关的文件。
-                    max_workers = self._file_concurrency,
+                    ],  # 常见 torch 相关文件 pattern。
+                    max_workers=self._file_concurrency,
                 )
             else:
                 # 服务器上可以选择这个，完全避免出错。
+                # 选择该方法可能会同步下载量化和编译后的模型，从而避免本地编译，加速推理服务化。
                 snapshot_download(
                     repo_id=repo_id,
                     repo_type='model',
@@ -139,23 +154,28 @@ class HFDownloader(HFDownloaderInterface):
                     cache_dir=self.cache_dir,
                     max_workers=self._file_concurrency,
                 )
-            logger.success(f"Save {repo_id} to {self.local_model_dir / repo_id}")
+            logger.success(f"Model: Save {repo_id} to {self.local_model_dir / repo_id}")
         except Exception as e:
-            logger.exception(f"{e}")
+            logger.exception(f"Model: \n{e}")
 
+    # ==== 重要方法。 ====
     def try_download_dataset(
         self,
         repo_id: str,
     ) -> None:
         """
-        从huggingface上下载数据集。
-        相比较下载模型，其实仅多了一个kwarg指定仓库类型为数据集。
+        从 huggingface 上下载数据集。
 
         Args:
-            repo_id: huggingface上仓库的id，一般是 "用户名/仓库名" ，可以自动复制的。
+            repo_id (str): huggingface 上仓库的 id ，一般是 "用户名/仓库名" ，可自动复制。
+
+        Returns:
+            None: 尝试执行下载数据集。
+                报错会以日志进行记录，但不会中断下载任务。
+                该方法约定被多线程方法批量运行，各下载任务独立进行，可重复启动和恢复。
         """
         try:
-            # 下载数据集
+            # 下载数据集。
             snapshot_download(
                 repo_id=repo_id,
                 repo_type='dataset',  # 如果是数据集
@@ -163,35 +183,48 @@ class HFDownloader(HFDownloaderInterface):
                 cache_dir=self.cache_dir,
                 max_workers=self._file_concurrency,
             )
-            logger.success(f"Save {repo_id} to {self.local_dataset_dir / repo_id}")
+            logger.success(f"Dataset: Save {repo_id} to {self.local_dataset_dir / repo_id}")
         except Exception as e:
-            logger.exception(f"{e}")
+            logger.exception(f"Dataset: \n{e}")
 
+    # ==== unstable ====
     def set_environment_variables(
         self,
         hf_endpoint: str | None,
         hf_home: str | None,
     ) -> None:
+        # 判别式加载 environment variables
+        ## 如果被设置，更新 env var 。
+        ## 如果为None，不覆盖全局设置。
         if hf_endpoint:
             # using a mirror
-            os.environ['HF_ENDPOINT'] = "https://hf-mirror.com"
+            os.environ['HF_ENDPOINT'] = hf_endpoint
         if hf_home:
-            os.environ['HF_HOME'] = "~/.cache/huggingface"
+            os.environ['HF_HOME'] = hf_home
 
 
-if __name__ == "__main__":
-    # downloader = HFDownloader(
-    #     local_model_dir=r"D:/model/",
-    #     local_dataset_dir=r"D:/dataset/",
-    #     is_only_torch=True,
-    #     # is_batch_download=True,
-    # )
-
-    # 仓库id。
+if __name__ == '__main__':
+    # 实例化下载器，约定统一配置。
+    downloader = HFDownloader(
+        local_model_dir=r"D:/model/",
+        local_dataset_dir=r"D:/dataset/",
+        cache_dir=None,  # 使用全局设置。
+        is_only_torch=True,
+        repo_concurrency=8,  # 可能会完全阻塞当前机器，根据情况再调整。
+        file_concurrency=32,  # 设置为 python 的最大，由 CPU 核心数量自动降低。
+        hf_endpoint="https://hf-mirror.com",  # 常用 mirror 之一。
+        hf_home=None,  # 使用全局设置。
+    )
+    # 设置目标仓库 id 。
     model_repo_ids = [r"Qwen/Qwen2.5-0.5B-Instruct", r"Qwen/Qwen2-VL-2B-Instruct"]
     dataset_repo_ids = [r"HuggingFaceTB/smoltalk"]
-    locaL_space_repo_ids = [r"HuggingFaceTB/smoltalk"]
-
-    # asyncio.run(downloader.download_models(model_repo_ids))
-    # asyncio.run(downloader.download_datasets(dataset_repo_ids))
+    # 以运行当前脚本的方法，批量执行下载。
+    ## models
+    downloader.download_models(
+        repo_ids=model_repo_ids,
+    )
+    ## datasets
+    downloader.download_datasets(
+        repo_ids=dataset_repo_ids,
+    )
 
